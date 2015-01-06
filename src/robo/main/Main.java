6d19a9cccea3adc61b5bbee04191fd000128f020
samutamm <samutamm@gmail.com>
2015-01-06 12:36:43 +0200

    Main ja Robot-luokan refaktorointia.

--- src/robo/main/Main.java
+++ src/robo/main/Main.java
@@ -12,22 +12,8 @@ public class Main {
 	
 	
 	public static void main(String[] args) {
-
-		Convoyer convoyer = new Convoyer(new MotorController(Motor.B));
-		Calculator calc = new Calculator();
-		Sorter sorter = new Sorter(new MotorController(Motor.A), calc);
-		LightSensor light = new LightSensor(SensorPort.S1);
-		Robot robo = new Robot(convoyer, sorter, light);
+		Robot robo = new Robot();
 
 		robo.run();
-		
-		LCD.clear();
-		LCD.drawString("Coin stats:", 0, 0);
-		List<String> coinStats = calc.getStatistics();
-		for(int i = 0; i < coinStats.size(); i++) {
-			LCD.drawString(coinStats.get(i), 0, i+1);
-		}
-
-		Button.waitForPress();
-	}
+	}	
 }
--- src/robo/main/Robot.java
+++ src/robo/main/Robot.java
@@ -1,54 +1,56 @@
 package robo.main;
 
+import java.util.List;
+
+import robo.Calculator.Calculator;
+import robo.contollers.MotorController;
 import robo.domain.*;
 import lejos.nxt.Button;
 import lejos.nxt.LCD;
 import lejos.nxt.LightSensor;
+import lejos.nxt.Motor;
+import lejos.nxt.SensorPort;
 import lejos.util.Delay;
 
 public class Robot {
 	
 	private Convoyer convoyer;
 	private Sorter sorter;
-	private LightSensor light;
+	private Calculator calc;
 	
-	public Robot(Convoyer convoyer, Sorter sorter, LightSensor light) {
-		this.convoyer = convoyer;
-		this.sorter = sorter;
-		this.light = light;
+	public Robot() {
+		this.calc = new Calculator();
+		this.sorter = new Sorter(new MotorController(Motor.A), calc, new LightSensor(SensorPort.S1));
+		this.convoyer = new Convoyer(new MotorController(Motor.B));
 	}
 	
 	
 	public void run() {
-		int emptyLightValue = findStartLightValue();
-		
 		LCD.drawString("Is coin ready?" , 0, 0);
 		Button.waitForPress();
 		
 		while(!Button.ESCAPE.isPressed()) { 
-			Coin coin = this.sorter.openPortUntilCoinFlops(2, emptyLightValue, this.light);
+			Coin coin = this.sorter.openPortUntilCoinFlops(2);
 			LCD.clear();
 			
 			this.convoyer.moveCoin(50, coin.convoyerAngles);
 			
 			LCD.drawString("Press Escape to stop or put an other coin and press Enter.", 0, 3);
 			Delay.msDelay(1000);
-			while(noNewCoinAdded() && Button.readButtons() == 0);
+			while(this.sorter.noNewCoinAdded() && Button.readButtons() == 0);
 		}
+		
+		this.printStatisticsAtEnd();
 	}
 	
-	private boolean noNewCoinAdded() {
-		Delay.msDelay(100);
-		return this.light.getLightValue() < 50;
-	}
-
-	public int findStartLightValue() {
-		int emptyLightValue = this.light.getLightValue();
-		Delay.msDelay(100);
-		while(emptyLightValue != this.light.getLightValue()) {
-			emptyLightValue = this.light.getLightValue();
-			Delay.msDelay(100);
+	public void printStatisticsAtEnd() {
+		LCD.clear();
+		LCD.drawString("Coin stats:", 0, 0);
+		List<String> coinStats = calc.getStatistics();
+		for(int i = 0; i < coinStats.size(); i++) {
+			LCD.drawString(coinStats.get(i), 0, i+1);
 		}
-		return emptyLightValue;
+
+		Button.waitForPress();
 	}
 }
